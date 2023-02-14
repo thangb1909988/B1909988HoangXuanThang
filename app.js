6efff946ca9ebcc4a29522148187e97f065745eb
thangb1909988 <113433167+thangb1909988@users.noreply.github.com>
2023-02-14 23:04:07 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
-
 const contactsRouter = require("./app/routes/contact.route");
+const ApiError = require("./app/api-error");
 
 const app = express();
 
@@ -12,5 +12,16 @@ app.use("/api/contacts", contactsRouter);
 app.get("/", (rep,res) => {
     res.json({message: "Welcome to contact book application."});
 });
+
+app.use((req, res, next) => {
+    return next(new ApiError(404, "Resource not found"));
+});
+
+app.use((err, req, res, next) => {
+    return res.status(error.statusCode || 500).json({
+        message: error.message || "Internal Server Error",
+    });
+});
+
 
 module.exports = app;
